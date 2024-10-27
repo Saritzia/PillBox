fd517c7fafbaad0307eb1fcf576861efe2a369ec
Sara Lopez Martinez <saritzia0988@gmail.com>
2024-10-27 12:47:34 +0100

    User table view modified to use router

--- PillBox/PillBox/Libraries/Users/Presentation/UserTableView.swift
+++ PillBox/PillBox/Libraries/Users/Presentation/UserTableView.swift
@@ -1,56 +1,51 @@
 import SwiftUI
 
 struct UserTableView: View {
+    @EnvironmentObject var router: Router
     @ObservedObject var userTableViewModel: UserTableViewModel
-    
-    init(userTableViewModel: UserTableViewModel) {
-        self.userTableViewModel = UserTableViewModel()
-    }
     private let constants = Constants()
     private var cells: [CellModel] {
         userTableViewModel.cellModels ?? []
     }
-
+    
+    init(userTableViewModel: UserTableViewModel) {
+        self.userTableViewModel = UserTableViewModel()
+    }
+    
     var body: some View {
-        NavigationStack {
-            List{
-                ForEach(cells) { cellModel in
-                    NavigationLink {
-                        DrugsTableView(drugTableViewModel: DrugsTableViewModel(userId: cellModel.id))
+        List(cells) { cellModel in
+            ReusableCell(cellModel: cellModel, delegate: userTableViewModel)
+                .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                    Button(role: .destructive) {
+                        userTableViewModel.onSwipe(id: cellModel.id)
                     } label: {
-                        ReusableCell(cellModel: cellModel, delegate: userTableViewModel)
-                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                            Button(role: .destructive) {
-                                userTableViewModel.onSwipe(id: cellModel.id)
-                            } label: {
-                                Label("Delete", systemImage: constants.trash).tint(.red)
-                            }
-                        }
-                        .frame(height: 50)
-                        .scaledToFit()
+                        Label("Delete", systemImage: constants.trash).tint(.red)
                     }
                 }
-            }
-            .toolbar(content: {
-                ToolbarItem(placement: .principal) {
-                    Text(String(localized: "users"))
-                        .colorInvert()
-                        .bold()
-                        .font(.title3)
+                .frame(height: 50)
+                .scaledToFill()
+                .onTapGesture {
+                    router.navigate(to: .drugTableView(id: cellModel.id))
                 }
-            })
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.visible, for: .navigationBar)
-            .toolbarBackground(.black, for: .navigationBar)
-            .listItemTint(.black)
-            .navigationBarBackButtonHidden(true)
-            .toolbar(content: {
-                ToolbarItem(placement: .topBarTrailing) {
-                    AddButton(alertTitle: String(localized: "userAlertTitle"), delegate: userTableViewModel)
-                }
-            })
         }
-        .tint(.white)
+        .toolbar(content: {
+            ToolbarItem(placement: .principal) {
+                Text(String(localized: "users"))
+                    .colorInvert()
+                    .bold()
+                    .font(.title3)
+            }
+        })
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbarBackground(.visible, for: .navigationBar)
+        .toolbarBackground(.black, for: .navigationBar)
+        .listItemTint(.black)
+        .navigationBarBackButtonHidden(true)
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarTrailing) {
+                AddButton(alertTitle: String(localized: "userAlertTitle"), delegate: userTableViewModel)
+            }
+        })
     }
 }
 
