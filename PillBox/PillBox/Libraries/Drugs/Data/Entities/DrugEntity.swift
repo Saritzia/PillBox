eb0a64c36ac5dffaf028d204b3fcf9ef28e66780
Sara Lopez Martinez <saritzia0988@gmail.com>
2024-11-16 11:49:10 +0100

    new parameters added to the drug entity

--- PillBox/PillBox/Libraries/Drugs/Data/Entities/DrugEntity.swift
+++ PillBox/PillBox/Libraries/Drugs/Data/Entities/DrugEntity.swift
@@ -2,16 +2,30 @@ import Foundation
 import RealmSwift
 
 class DrugEntity: Object, Identifiable {
-    @Persisted(primaryKey: true) var _idDrug : ObjectId
-    @Persisted var drugName : String
+    @Persisted(primaryKey: true) var _idDrug: ObjectId
+    @Persisted var drugName:  String?
     @Persisted var avatar: String
-    @Persisted var date : Date
+    @Persisted var createdDate: Date
+    @Persisted var endDate: Date?
+    @Persisted var isToogleOn: Bool
+    @Persisted var numberOfTimes: Int?
+    @Persisted var timeUnit: Int?
+    @Persisted var otherInformation: String?
     
-    convenience init(drugName: String) {
+    convenience init(drugName: String?,
+                     endDate: Date? = nil,
+                     isToogleOn: Bool = false,
+                     numberOfTimes: Int? = nil,
+                     timeUnit: Int? = nil,
+                     otherInformation: String? = nil) {
         self.init()
         self.drugName = drugName
-        self.date = Date()
+        self.createdDate = Date()
         self.avatar = "pillAvatar"
+        self.endDate = endDate
+        self.isToogleOn = isToogleOn
+        self.numberOfTimes = numberOfTimes
+        self.timeUnit = timeUnit
+        self.otherInformation = otherInformation
     }
-    // @Persisted var configuration : ConfigurationModel?
 }
