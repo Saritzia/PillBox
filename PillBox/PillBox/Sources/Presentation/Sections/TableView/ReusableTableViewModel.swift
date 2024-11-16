aa1e7c525020dad3aa2cab732f8f814149619235
Sara Lopez Martinez <saritzia0988@gmail.com>
2024-11-16 11:52:42 +0100

    Mainactor

--- PillBox/PillBox/Sources/Presentation/Sections/TableView/ReusableTableViewModel.swift
+++ PillBox/PillBox/Sources/Presentation/Sections/TableView/ReusableTableViewModel.swift
@@ -2,6 +2,6 @@ import Foundation
 
 protocol ReusableTableViewModelContract: ObservableObject {
     var cellModels: [CellModel]? { get set }
-    func fetchData()
+    @MainActor func fetchData()
 }
 
