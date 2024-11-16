f330adc398b62428e8cf3d605134ce21e6487154
Sara Lopez Martinez <saritzia0988@gmail.com>
2024-11-16 11:47:56 +0100

    drug model with new parameters

--- PillBox/PillBox/Libraries/Drugs/Domain/Models/DrugModel.swift
+++ PillBox/PillBox/Libraries/Drugs/Domain/Models/DrugModel.swift
@@ -1,8 +1,33 @@
 import Foundation
 
 struct DrugModel: Equatable {
-    let idDrug : String
-    let drug: String
-    let avatar: String
-    let date: Date
+    let idDrug : String?
+    let drug: String?
+    let avatar: String?
+    let createdDate: Date?
+    let endDate: Date?
+    let isToogleOn: Bool
+    let numberOfTimes: Int?
+    let timeUnit: Int?
+    let otherInformation: String?
+    
+    init(idDrug: String? = nil,
+         drug: String?,
+         avatar: String? = nil,
+         createdDate: Date? = nil,
+         endDate: Date? = nil,
+         isToogleOn: Bool = false,
+         numberOfTimes: Int? = nil,
+         timeUnit: Int? = nil,
+         otherInformation: String? = nil) {
+        self.idDrug = idDrug
+        self.drug = drug
+        self.avatar = avatar
+        self.createdDate = createdDate
+        self.endDate = endDate
+        self.isToogleOn = isToogleOn
+        self.numberOfTimes = numberOfTimes
+        self.timeUnit = timeUnit
+        self.otherInformation = otherInformation
+    }
 }
