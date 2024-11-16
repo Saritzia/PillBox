d54fdff9ba455db1a6d44103463fd1e96aa3da0f
Sara Lopez Martinez <saritzia0988@gmail.com>
2024-11-16 11:48:50 +0100

    update configuration added to the repository

--- PillBox/PillBox/Libraries/Drugs/Data/Repositories/DrugsRepository.swift
+++ PillBox/PillBox/Libraries/Drugs/Data/Repositories/DrugsRepository.swift
@@ -2,9 +2,10 @@ import RealmSwift
 
 protocol DrugsRepositoryContract {
     func fetchDrugs(user: String) -> [DrugModel]
-    func saveData(drug: String, user: String) throws
+    func saveData(drug: DrugModel, user: String) throws
     func deleteData(_ id: String, user: String) throws
     func updateData(avatar: String, id: String, user: String) throws
+    func updateConfiguration(user: String, drug: DrugModel) throws
 }
 
 final class DrugsRepository: DrugsRepositoryContract {
@@ -15,19 +16,42 @@ final class DrugsRepository: DrugsRepositoryContract {
             DrugModel(idDrug: entity._idDrug.stringValue,
                       drug: entity.drugName,
                       avatar: entity.avatar,
-                      date: entity.date)
+                      createdDate: entity.createdDate,
+                      endDate: entity.endDate,
+                      isToogleOn: entity.isToogleOn,
+                      numberOfTimes: entity.numberOfTimes,
+                      timeUnit: entity.timeUnit,
+                      otherInformation: entity.otherInformation)
         } ?? []
     }
 
-    func saveData(drug: String, user: String) throws {
+    func saveData(drug: DrugModel, user: String) throws {
         try realm.write {
-            realm.objects(UserEntity.self).filter({ $0._idUser.stringValue == user }).first?.drugs.append(DrugEntity(drugName: drug))
+            realm.objects(UserEntity.self).filter({ $0._idUser.stringValue == user })
+                .first?.drugs.append(DrugEntity(drugName: drug.drug,
+                                                endDate: drug.endDate,
+                                                isToogleOn: drug.isToogleOn,
+                                                numberOfTimes: drug.numberOfTimes,
+                                                timeUnit: drug.timeUnit,
+                                                otherInformation: drug.otherInformation))
         }
     }
     
     func updateData(avatar: String, id: String, user: String) throws {
         try realm.write {
             realm.objects(UserEntity.self).filter({ $0._idUser.stringValue == user }).first?.drugs.filter { $0._idDrug.stringValue == id }.first?.avatar = avatar
+        }
+    }
+    
+    func updateConfiguration(user: String, drug: DrugModel) throws {
+        let selectedDrug = realm.objects(UserEntity.self).filter({ $0._idUser.stringValue == user })
+            .first?.drugs.filter { $0._idDrug.stringValue == drug.idDrug }.first
+        try realm.write {
+            selectedDrug?.endDate = drug.endDate
+            selectedDrug?.isToogleOn = drug.isToogleOn
+            selectedDrug?.numberOfTimes = drug.numberOfTimes
+            selectedDrug?.timeUnit = drug.timeUnit
+            selectedDrug?.otherInformation = drug.otherInformation
         }
     }
     
